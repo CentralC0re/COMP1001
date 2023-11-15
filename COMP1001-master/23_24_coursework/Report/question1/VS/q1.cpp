9117d8b1a70d2aab1ad20245128844f2c710735c
CentralC0re <theepicgameminer@gmail.com>
2023-11-15 15:33:21 +0000

    Routines done, error check started
    Error check currently uses a hardcoded value, which does not agree with the criteria specifying it must work with any input size.
    Routines 1 and 2 must be modified so that they do not edit y and w, but instead results arrays.

--- COMP1001-master/23_24_coursework/Report/question1/VS/q1.cpp
+++ COMP1001-master/23_24_coursework/Report/question1/VS/q1.cpp
@@ -28,6 +28,9 @@
 void initialize();
 void routine1(float alpha, float beta);
 void routine2(float alpha, float beta);
+void routine1_vec(float alpha, float beta);
+void routine2_vec(float alpha, float beta);
+void checkValues(float* correct, float* checking);
 
 __declspec(align(64)) float  y[M], z[M] ;
 __declspec(align(64)) float A[N][N], x[N], w[N];
@@ -40,15 +43,38 @@ int main() {
 
     initialize();
 
-    printf("\nRoutine1:");
-    start_time = omp_get_wtime(); //start timer
+	//R1	-	Commented out so that y remains in its unmodified state.
+	//			Unless I edit Routine1 so it stores its results elsewhere, it must remain commented out
+	//			FIX THIS - "Your program must work for any input size value (any ‘N, M’ values)." Results cannot be hardcoded.
+    //printf("\nRoutine1:");
+    //start_time = omp_get_wtime(); //start timer
+
+    //for (t = 0; t < TIMES1; t++)
+    //    routine1(alpha, beta);
+
+    //run_time = omp_get_wtime() - start_time; //end timer
+    //printf("\n Time elapsed is %f secs \n %e FLOPs achieved\n", run_time, (double)(ARITHMETIC_OPERATIONS1) / ((double)run_time / TIMES1));
+    
+	float r1CheckValues[5] = {0.0660100058, 0.451010048, 0.478009999, 0.704010010, 0.772009969};	// Hard coded values from routine1
+																									// Temporary, please fix
+	//R1_V
+	printf("\nRoutine1 Vectorised:");
+	start_time = omp_get_wtime(); //start timer
 
     for (t = 0; t < TIMES1; t++)
-        routine1(alpha, beta);
+        routine1_vec(alpha, beta);
 
     run_time = omp_get_wtime() - start_time; //end timer
     printf("\n Time elapsed is %f secs \n %e FLOPs achieved\n", run_time, (double)(ARITHMETIC_OPERATIONS1) / ((double)run_time / TIMES1));
 
+	float r1VCheckValues[5] = { y[1], y[25], y[300], y[1024], y[1025] }; // Check values from the result of routine1_vec
+																		// These may be incorrect.
+
+	checkValues(r1CheckValues, r1VCheckValues);
+
+
+
+	//R2
     printf("\nRoutine2:");
     start_time = omp_get_wtime(); //start timer
 
@@ -59,6 +85,15 @@ int main() {
     printf("\n Time elapsed is %f secs \n %e FLOPs achieved\n", run_time, (double)(ARITHMETIC_OPERATIONS2) / ((double)run_time / TIMES2));
 
 
+	//R2_V
+	printf("\nRoutine2 Vectorised:");
+	start_time = omp_get_wtime(); //start timer
+
+	for (t = 0; t < TIMES2; t++)
+		routine2_vec(alpha, beta);
+
+	run_time = omp_get_wtime() - start_time; //end timer
+	printf("\n Time elapsed is %f secs \n %e FLOPs achieved\n", run_time, (double)(ARITHMETIC_OPERATIONS2) / ((double)run_time / TIMES2));
 
     return 0;
 }
@@ -93,6 +128,16 @@ void initialize() {
 
 void routine1(float alpha, float beta) {
 
+	unsigned int i;
+
+
+	for (i = 0; i < M; i++)
+		y[i] = alpha * y[i] + beta * z[i];
+
+}
+
+void routine1_vec(float alpha, float beta) {
+
     unsigned int i;
 	float aArray[8];
 	float bArray[8];
@@ -106,9 +151,11 @@ void routine1(float alpha, float beta) {
 	// Attempted to manually destroy a and b arrays, but it's far too much effort for
 	// very little memory saving.
 
+	__m256 finalResult;
+
 	for (i = 0; i < M; i += 8)
-	{	// Why were these curly brackets missing?
-		__m256 aResult, bResult, finalResult;		// Variables defined here so they go out of scope on loop end.
+	{	// Curly brackets were missing and have been added.
+		__m256 aResult, bResult;		// Variables defined here so they go out of scope on loop end.
 		__m256 yContain = _mm256_loadu_ps(&y[i]);
 		__m256 zContain = _mm256_loadu_ps(&z[i]);
 		aResult = _mm256_mul_ps(aContain, yContain);
@@ -132,6 +179,57 @@ void routine2(float alpha, float beta) {
 
 }
 
+void routine2_vec(float alpha, float beta) {
 
+	unsigned int i, j;
+	float aArray[8];
+	float bArray[8];
+	for (int i = 0; i < 8; i++) {	// Probably inefficient.
+		aArray[i] = alpha;
+		bArray[i] = beta;
+	}
 
+	__m256 alphaContain = _mm256_loadu_ps(aArray);
+	__m256 betaContain = _mm256_loadu_ps(bArray);
+
+	for (i = 0; i < N; i+=8)
+	{
+		unsigned int iArray[8] = { i };	// Despite both being initialised as int, i is apparently unsigned
+										// Set array to unsigned to remove conversion warning and allow greater upper value
+		__m256 wContain = _mm256_loadu_ps(&w[i]);
+		__m256 subResult = _mm256_sub_ps(wContain, betaContain);	// This does not need to be recalculated
+		// alpha cannot be added before the loop because of the order of operations (alpha * A)
+		for (j = 0; j < N; j+=8)									// every j loop
+		{
+			__m256 arrAContain = _mm256_loadu_ps(&A[i][j]);
+			__m256 xContain = _mm256_loadu_ps(&x[j]);
+
+			__m256 multResult = _mm256_mul_ps(_mm256_mul_ps(alphaContain, arrAContain), xContain);	// Saves a variable
+
+			__m256 finalResult = _mm256_add_ps(subResult, multResult);
+			_mm256_storeu_ps(&w[i], finalResult);
+
+			//w[i] = w[i] - beta + alpha * A[i][j] * x[j];
+		}
+	}
+}
+
+void checkValues(float* correct, float* checking) {
+	bool isCorrect = true;
+	for (int i = 0; i < 5; i++)	// Fixed i cap as the arrays are always length 5
+	{
+		if (abs((correct[i] - checking[i]) / checking[i]) >= 0.00001)	// FP calculation to determine if two values are equal enough
+		{
+			isCorrect = false;
+		}
+	}
+
+	printf("");
+	if (isCorrect) {
+		printf("Values match.\n");
+	}
+	else {
+		printf("Error: Values do not match.\n");
+	}
+}
 
